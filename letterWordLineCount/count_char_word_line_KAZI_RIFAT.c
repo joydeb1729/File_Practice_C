06b490f624754fc0ba54edbed580f88180601d67
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2024-04-17 21:17:59 +0600

    added solutions to ques 3 and 4

---
+++ letterWordLineCount/count_char_word_line_KAZI_RIFAT.c
@@ -0,0 +1,60 @@
+/*
+**file practice question**
+@author_Durjay
+solution by KAZI RIFAT MORSHED 230220
+
+4. Write a C program to count characters, words and lines in a text file.
+
+input->
+I love   programming.
+Working   with files in C programming is fun.
+I am learning  C  programming   at KU.
+
+Output->
+Total characters = 106
+Total words      = 18
+Total lines      = 3
+*/
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+  FILE *f1 = fopen("file4.txt", "r");
+  if (f1 == NULL) {
+    printf("ERROR OPENING FILE");
+    exit(EXIT_FAILURE);
+  }
+  int char_count = 0, word_count = 0, line_count = 1, temp = 0;
+
+  // character counting
+  fseek(f1, 0, SEEK_END);
+  char_count = ftell(f1);
+  rewind(f1);
+
+  // char str[char_count + 1];
+  char *str = (char *)malloc(sizeof(char) * char_count);
+  char c;
+  while ((c = fgetc(f1)) != EOF) {
+    str[temp] = c;
+    if (c == '\n') {
+      line_count++;
+    }
+    temp++;
+  }
+
+  // word  count
+  char *t = strtok(str, " .,/*-+;':\" {}()[]\\|`~!@#$\%^&*");
+  while (t != NULL) {
+    word_count++;
+    t = strtok(NULL, " .,/*-+;':\" {}()[]\\|`~!@#$\%^&*");
+  }
+
+  // result
+  printf("Total characters\t= %d\n"
+         "Total words\t\t= %d\n"
+         "Total lines\t\t= %d\n",
+         char_count, word_count, line_count);
+  fclose(f1);
+} // DONE
